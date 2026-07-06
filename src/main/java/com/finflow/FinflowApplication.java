cf23b8032c0310e1ae39df5af10394bfc359e2c4
subodhin <nsubdohi@gmail.com>
2026-07-06 18:51:57 +0530

    day 9 security config add base done

--- src/main/java/com/finflow/FinflowApplication.java
+++ src/main/java/com/finflow/FinflowApplication.java
@@ -9,16 +9,7 @@ public class FinflowApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinflowApplication.class, args);
 
-        //coding Qs for interviews
-        //reverse a string!
-        String str = "hello";
-        String reversed ="";
 
-        for(int i= str.length() -1;i>=0;i--){
-            reversed += str.charAt(i);
-        }
-
-        System.out.println("Reversed string: " + reversed);
     }
 
 }
